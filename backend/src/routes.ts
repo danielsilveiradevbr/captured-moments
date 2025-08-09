cfaf1b92ea8bac15d42b39a82bea368f03571d80
danielzinhors <danielzinhors@gmail.com>
2025-08-09 18:21:15 -0300

    Ajuste na rota de moments

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,14 +4,9 @@ import { LoginUserController } from "./controller/auth/LoginUserController";
 import { GetUserController } from "./controller/auth/GetUserController";
 import { authenticateToken } from "./middleware/authenticateToken";
 import prismaClient from "./prisma";
+import { AddRegisteredMomentsController } from "./controller/moments/AddRegisteredMoments";
 
-interface RegisteredMomentsProps {
-  title: string
-  story: string
-  visitedLocation: string[]
-  imageUrl: string
-  visitedDate: string
-}
+
 export function routes(fastify: FastifyInstance){
     fastify.post('/create-account',async (request, reply) => {
        return new CreateUserController().handle(request, reply)                           
@@ -26,42 +21,6 @@ export function routes(fastify: FastifyInstance){
     })
 
     fastify.post('/add-registered-moments', {preHandler: authenticateToken}, async(request, reply) => {
-      const {title,
-      story,
-      visitedLocation,
-      imageUrl,
-      visitedDate} = request.body as RegisteredMomentsProps
-      const {user} = request
-
-      if(!title || !story || !visitedLocation || !imageUrl || !visitedDate) {
-        return reply.status(400).send({
-          error: true,
-          message: 'Todos os campos são obrigatórios!'
-        })      
-      }
-
-      if(!user) {
-        return reply.status(400).send({
-          error: true,
-          message: 'Usuário não encontrado!'
-        })      
-      }
-
-      const parsevisitedDate = new Date(parseInt(visitedDate))
-
-
-
-      const registeredMoments = await prismaClient.registeredMoment.create({
-        data: {
-          title: title,
-          story: story,
-          visitedLocation: visitedLocation,
-          imageUrl: imageUrl,
-          visitedDate: parsevisitedDate,
-          userId: user.userId
-        }
-      })
-      
-      reply.status(201).send({moment: registeredMoments})
+      return new AddRegisteredMomentsController().handle(request, reply)     
     })  
 }
